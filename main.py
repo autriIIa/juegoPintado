5e7aec9f33037f7f2fa8953d83128bf3e3c30b76
Arturo Utrilla Hernández <itsart.tw@gmail.com>
2025-03-20 13:37:11 -0600

    Funciones para crear un rectangulo y un triangulo

--- main.py
+++ main.py
@@ -31,12 +31,32 @@ def circle(start, end):
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(4):
+        if count % 2 == 1:
+            forward((end.x - start.x) / 2)
+        else:
+            forward(end.x - start.x)
+        left(90)
+
+    end_fill()
 
 
 def triangle(start, end):
-    "Draw triangle from start to end."
-    pass  # TODO
+    "Draw circle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+    end_fill()
 
 
 def tap(x, y):
@@ -57,7 +77,7 @@ def store(key, value):
     state[key] = value
 
 
-state = {'start': None, 'shape': line}
+state = {'start': None, 'shape': triangle}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
 listen()
